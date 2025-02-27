a9909531fd7cd2a5804e3986a84438193d2331b7
Sebastian Paulis <sebastian4paulis@gmail.com>
2025-02-27 18:37:24 -0500

    fix: Resolve layouts and resizings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Container, Stack } from '@mui/material';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
-        <body style={{margin: 0, padding: 0}}>
-        <Box>
-            <AppBar position="static" sx={{backgroundColor: '#add8e6'}}>
-                <Toolbar>
-                    <Typography variant="h6" sx={{flexGrow: 1, color: '#003366'}}>
-                        Connections & Directions
-                    </Typography>
-                    <Button sx={{
-                        color: '#fff',
-                        backgroundColor: '#003366',
-                        '&:hover': {backgroundColor: '#001f4d'}
-                    }}>LOGIN</Button>
-                </Toolbar>
-            </AppBar>
-            <Container sx={{marginTop: 4}}>{children}</Container>
-        </Box>
-        </body>
+            <body style={{ margin: 0, padding: 0 }}>
+                <AppRouterCacheProvider>
+                    <Box sx={{ width: '100dvw', height: '100dvh' }}>
+                        <AppBar position="static" sx={{ backgroundColor: '#add8e6', height: '64px' }}>
+                            <Toolbar>
+                                <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{width: 1}}>
+                                    <Typography variant="h6" sx={{ color: '#003366' }}>
+                                        Connections & Directions
+                                    </Typography>
+                                    <Button sx={{
+                                        color: '#fff',
+                                        backgroundColor: '#003366',
+                                        '&:hover': { backgroundColor: '#001f4d' }
+                                    }}>LOGIN</Button>
+                                </Stack>
+                            </Toolbar>
+                        </AppBar>
+                        <Box sx={{ width: 1, height: 'calc(100% - 64px)' }}>{children}</Box>
+                    </Box>
+                </AppRouterCacheProvider>
+            </body>
         </html>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import {Paper, Grid, Typography, Button, Box} from '@mui/material';
+import { Paper, Grid, Typography, Button, Box, Container } from '@mui/material';
 import { useRouter } from 'next/navigation';
 
 const WelcomePage = () => {
@@ -11,69 +11,71 @@ const WelcomePage = () => {
     };
 
     return (
-        <Box>
-            <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ minHeight: '80vh' }}>
-                <Grid item xs={6}>
-                    <Paper elevation={3} sx={{ padding: 4, textAlign: 'center', minWidth: 250, boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                        <Typography variant="h4" gutterBottom>
-                            Welcome to
-                        </Typography>
-                        <Typography variant="h3" fontWeight="bold">
-                            Connections
-                        </Typography>
-                        <Typography variant="h3" fontWeight="bold">
-                            &
-                        </Typography>
-                        <Typography variant="h3" fontWeight="bold">
-                            Directions
-                        </Typography>
-                    </Paper>
+        <Box sx={{ width: 1, height: 1, overflow: 'auto' }}>
+            <Container>
+                <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ minHeight: '80vh' }}>
+                    <Grid item xs={6}>
+                        <Paper elevation={3} sx={{ padding: 4, textAlign: 'center', minWidth: 250, boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                            <Typography variant="h4" gutterBottom>
+                                Welcome to
+                            </Typography>
+                            <Typography variant="h3" fontWeight="bold">
+                                Connections
+                            </Typography>
+                            <Typography variant="h3" fontWeight="bold">
+                                &
+                            </Typography>
+                            <Typography variant="h3" fontWeight="bold">
+                                Directions
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper elevation={3} sx={{ padding: 4, maxWidth: 400, boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                            <Typography>
+                                Connections & Directions is a free diagramming website that allows students to create ERD diagrams using Chen's and Crow's foot notation. Created by Nicholas Hartog and Sebastian Paulis, we guarantee the diagrams are accurate to their notation style for academic use by professors. Freely hosted on Firebase, anyone can use this service without any fear. Users can store their diagrams online or download them locally for use in classes or shared online. Feel free to reach out and contact us!
+                            </Typography>
+                            <Button variant="contained" color="primary" sx={{ marginTop: 2, backgroundColor: '#1976d2' }} onClick={handleEnterClick}>
+                                Enter
+                            </Button>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                    <Paper elevation={3} sx={{ padding: 4, maxWidth: 400, boxShadow: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                        <Typography>
-                            Connections & Directions is a free diagramming website that allows students to create ERD diagrams using Chen's and Crow's foot notation. Created by Nicholas Hartog and Sebastian Paulis, we guarantee the diagrams are accurate to their notation style for academic use by professors. Freely hosted on Firebase, anyone can use this service without any fear. Users can store their diagrams online or download them locally for use in classes or shared online. Feel free to reach out and contact us!
-                        </Typography>
-                        <Button variant="contained" color="primary" sx={{ marginTop: 2, backgroundColor: '#1976d2' }} onClick={handleEnterClick}>
-                            Enter
-                        </Button>
-                    </Paper>
-                </Grid>
-            </Grid>
 
-            {/* New Section Below */}
-            <Box sx={{ marginTop: 6 }}>
-                <Grid container spacing={4} justifyContent="center">
-                    <Grid item xs={5}>
-                        <Paper elevation={3} sx={{ padding: 4 }}>
-                            <Typography variant="h5" fontWeight="bold" gutterBottom>
-                                Chen's Notation
-                            </Typography>
-                            <Typography>
-                                Chens Notation Yet remarkably appearance get him his projection. Diverted endeavor bed peculiar men the not desirous.
-                                Acuteness abilities ask can offending furnished fulfilled sex.
-                            </Typography>
-                            <Typography sx={{ marginTop: 2 }}>
-                                Link to learn more.
-                            </Typography>
-                        </Paper>
+                {/* New Section Below */}
+                <Box sx={{ marginTop: 6 }}>
+                    <Grid container spacing={4} justifyContent="center">
+                        <Grid item xs={5}>
+                            <Paper elevation={3} sx={{ padding: 4 }}>
+                                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                                    Chen's Notation
+                                </Typography>
+                                <Typography>
+                                    Chens Notation Yet remarkably appearance get him his projection. Diverted endeavor bed peculiar men the not desirous.
+                                    Acuteness abilities ask can offending furnished fulfilled sex.
+                                </Typography>
+                                <Typography sx={{ marginTop: 2 }}>
+                                    Link to learn more.
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Paper elevation={3} sx={{ padding: 4 }}>
+                                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                                    Crow's Foot Notation
+                                </Typography>
+                                <Typography>
+                                    Crow's Foot Notation Yet remarkably appearance get him his projection. Diverted endeavor bed peculiar men the not desirous.
+                                    Acuteness abilities ask can offending furnished fulfilled sex.
+                                </Typography>
+                                <Typography sx={{ marginTop: 2 }}>
+                                    Link to learn more.
+                                </Typography>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={5}>
-                        <Paper elevation={3} sx={{ padding: 4 }}>
-                            <Typography variant="h5" fontWeight="bold" gutterBottom>
-                                Crow's Foot Notation
-                            </Typography>
-                            <Typography>
-                                Crow's Foot Notation Yet remarkably appearance get him his projection. Diverted endeavor bed peculiar men the not desirous.
-                                Acuteness abilities ask can offending furnished fulfilled sex.
-                            </Typography>
-                            <Typography sx={{ marginTop: 2 }}>
-                                Link to learn more.
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Box>
+                </Box>
+            </Container>
         </Box>
     );
 };
