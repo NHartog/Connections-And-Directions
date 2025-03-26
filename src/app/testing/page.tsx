2865a0d28f67ceb91d8fc7db7724f643bf6602ea
Sebastian Paulis <sebastian4paulis@gmail.com>
2025-03-26 18:49:23 -0400

    feat: Add better smart routing

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,10 +1,13 @@
 'use client'
-import { Box, Button, SpeedDial, SpeedDialAction, SpeedDialIcon, Stack } from '@mui/material';
+import { Box, Button, IconButton, Popover, SpeedDial, SpeedDialAction, SpeedDialIcon, Stack, Tooltip, Typography } from '@mui/material';
 import { dia, shapes, highlighters, elementTools, linkTools, util } from '@joint/core';
 import * as joint from '@joint/core';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { AvoidRouter } from './avoid-router';
 import SaveIcon from "@mui/icons-material/Save";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import DescriptionIcon from '@mui/icons-material/Description';
 import React from "react";
 
 import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef } from "react-zoom-pan-pinch";
@@ -14,8 +17,9 @@ export default function Home() {
   const [graph, setGraph] = useState<dia.Graph>()
   const [panningEnabled, setPanningEnabled] = useState<boolean>(true)
   const [selectionManager, setSelectionManager] = useState<SelectionManager>();
+  const [helpAnchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const isHelpOpen = Boolean(helpAnchorEl);
   const fileInputRef = useRef<HTMLInputElement>(null);
-
   const shiftHeld = useRef<boolean>(false)
   const transformWrapperRef = useRef<ReactZoomPanPinchRef>(null)
   const boxWrapperRef = useRef<HTMLElement>(null)
@@ -23,6 +27,15 @@ export default function Home() {
   const height = 8000 // in pixels
   const elementWidth = 150 // in pixels
   const elementHeight = 50 // in pixels
+
+  const handleHelpClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleHelpClose = () => {
+    setAnchorEl(null);
+  };
+
 
   const onImport = () => {
     if (fileInputRef.current) {
@@ -184,9 +197,9 @@ export default function Home() {
           sourceMarker: { type: 'none' }    // No arrowhead at source
         }
       },
-      router: {
-        name: 'orthogonal'
-      },
+      //router: {
+      //  name: 'orthogonal'
+      //},
       connector: {
         name: 'rounded'
       }
@@ -209,9 +222,9 @@ export default function Home() {
           strokeWidth: 10,       // Thickness of the outline (should be greater than strokeWidth)
         }
       },
-      router: {
-        name: 'orthogonal'
-      },
+      //router: {
+      //  name: 'orthogonal'
+      //},
       connector: {
         name: 'rounded'
       }
@@ -222,6 +235,7 @@ export default function Home() {
   useEffect(() => {
     // create paper
     const namespace = shapes;
+
 
     const graph = new dia.Graph({}, { cellNamespace: namespace });
     setGraph(graph)
@@ -237,8 +251,8 @@ export default function Home() {
         thickness: 1,
         name: 'mesh',
       },
-      interactive: true,
       linkPinning: false,
+      interactive: { linkMove: false },
       snapLinks: { radius: 10 },
       defaultLink: doubleLine,
       linkLayer: 0,
@@ -294,6 +308,7 @@ export default function Home() {
     paper.on('link:mouseleave', (linkView) => {
       linkView.removeTools();
     });
+
     paper.on('element:mouseenter', elementView => {
       elementView.showTools();
     });
@@ -302,6 +317,97 @@ export default function Home() {
       elementView.hideTools();
     });
 
+    const routerWorker = new Worker(new URL("./worker.js", import.meta.url));
+
+    routerWorker.onmessage = (e) => {
+      const { command, ...data } = e.data;
+      switch (command) {
+        case 'routed': {
+          const { cells } = data;
+          cells.forEach((cell: joint.dia.Link) => {
+            const model = graph.getCell(cell.id);
+            if (model.isElement()) return;
+            model.set({
+              vertices: cell.vertices,
+              source: cell.source,
+              target: cell.target,
+              router: null
+            }, {
+              fromWorker: true
+            });
+          });
+          highlighters.addClass.removeAll(paper, 'awaiting-update');
+          break;
+        }
+        default:
+          console.log('Unknown command', command);
+          break;
+      }
+    }
+
+    routerWorker.postMessage([{
+      command: 'reset',
+      cells: graph.toJSON().cells
+    }]);
+
+    graph.on('change', (cell, opt) => {
+
+      if (opt.fromWorker) {
+        return;
+      }
+
+      routerWorker.postMessage([{
+        command: 'change',
+        cell: cell.toJSON()
+      }]);
+
+      if (cell.isElement() && (cell.hasChanged('position') || cell.hasChanged('size'))) {
+        const links = graph.getConnectedLinks(cell);
+        links.forEach((link) => {
+          link.router() || link.router('rightAngle');
+          highlighters.addClass.add(link.findView(paper), 'line', 'awaiting-update', {
+            className: 'awaiting-update'
+          });
+        });
+      }
+
+    });
+
+    graph.on('remove', (cell) => {
+      routerWorker.postMessage([{
+        command: 'remove',
+        id: cell.id
+      }]);
+    });
+
+    graph.on('add', (cell) => {
+      routerWorker.postMessage([{
+        command: 'add',
+        cell: cell.toJSON()
+      }]);
+    });
+
+    //paper.on('link:snap:connect', (linkView) => {
+    //    linkView.model.router('rightAngle');
+    //});
+
+    paper.on('link:snap:disconnect', (linkView) => {
+      linkView.model.set({
+        vertices: [],
+        router: null
+      });
+    });
+
+    AvoidRouter.load().then(() => {
+      const router = new AvoidRouter(graph, {
+        shapeBufferDistance: 20,
+        idealNudgingDistance: 10,
+        portOverflow: 8,
+      });
+
+      router.addGraphListeners();
+      router.routeAll();
+    });
 
     setPaper(paper)
   }, [])
@@ -462,7 +568,7 @@ export default function Home() {
   return (
     <Stack direction="row" sx={{ width: '100%', height: '100%' }}>
       <Box sx={{ width: 'calc(100% - 300px)', height: '100%' }}>
-        <Box sx={{ position: 'fixed', zIndex: 2 }}>
+        <Stack direction="row" spacing={1} sx={{ position: 'fixed', zIndex: 2, p: 1 }}>
           <Button variant="contained" onClick={() => transformWrapperRef.current?.zoomIn()}>+</Button>
           <Button variant="contained" onClick={() => transformWrapperRef.current?.zoomOut()}>-</Button>
           <Button variant="contained" onClick={() => transformWrapperRef.current?.resetTransform()}>x</Button>
@@ -470,6 +576,50 @@ export default function Home() {
           <Button onClick={() => addElement('attribute', new shapes.standard.Ellipse())} variant="contained">Add Attribute</Button>
           <Button onClick={() => addElement('entity', new shapes.standard.Rectangle())} variant="contained">Add Entity</Button>
           <Button onClick={() => addElement('relationship', new shapes.standard.Polygon())} variant="contained">Add Relationship</Button>
+        </Stack>
+        <Box sx={{ position: 'fixed', zIndex: 2, right: 316, paddingTop: 1 }}>
+          <Tooltip title="Click for help" arrow>
+            <IconButton color="primary" onClick={handleHelpClick} sx={{
+              backgroundColor: 'primary.main', // Solid color from your theme
+              color: 'white', // Icon color
+              '&:hover': {
+                backgroundColor: 'primary.dark', // Darker color on hover
+              },
+            }}>
+              <HelpOutlineIcon />
+            </IconButton>
+          </Tooltip>
+          <Popover
+            open={isHelpOpen}
+            anchorEl={helpAnchorEl}
+            onClose={handleHelpClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+          >
+            <Typography sx={{ px: 2, maxWidth: 400 }} component="div">
+              <h3>Help Guide</h3>
+              <p><b>Zooming:</b> You can zoom in and out using the <i>+</i> and <i>-</i> buttons, or reset zoom with the <i>Reset</i> button.</p>
+              <p><b>Panning:</b> Use right click (or hold your mouse wheel) while dragging to move the entire diagram.</p>
+              <p><b>Element Actions:</b></p>
+              <ul>
+                <li><b>Add Attribute:</b> Add a new attribute element.</li>
+                <li><b>Add Entity:</b> Add a new entity element.</li>
+                <li><b>Add Relationship:</b> Add a new relationship element.</li>
+              </ul>
+              <p><b>Element Selection:</b> Click and drag to select multiple elements.</p>
+              <ul>
+                <li>You can move selected elements by dragging them together.</li>
+                <li>You can use <b>Shift</b> to multi-select.</li>
+                <li>Hitting <b>Delete</b> or <b>Backspace</b> will delete the selected elements</li>
+                <li>Hitting <b>Escape</b> will clear the selection</li>
+              </ul>
+              <p><b>File Operations:</b></p>
+              <ul>
+                <li><b>Save:</b> Save the current diagram as a JSON file.</li>
+                <li><b>Import:</b> Import a diagram by uploading a JSON file.</li>
+              </ul>
+            </Typography>
+          </Popover>
         </Box>
         <Box sx={{ position: 'fixed', zIndex: 2, bottom: 16, right: 316 }}>
           <input
@@ -481,7 +631,7 @@ export default function Home() {
           />
           <SpeedDial
             ariaLabel="SpeedDial actions"
-            icon={<SpeedDialIcon />}
+            icon={<DescriptionIcon />}
           >
             <SpeedDialAction
               icon={<SaveIcon />}
@@ -532,7 +682,7 @@ export default function Home() {
           </TransformWrapper>
         </Box>
       </Box>
-      <Box sx={{ backgroundColor: 'red', width: '300px', height: '100%' }}>
+      <Box sx={{ backgroundColor: 'white', width: '300px', height: '100%' }}>
 
       </Box>
     </Stack>
